a2496ab16d0becba4298ae22cc2b3d07ae90981a
MichaelKarnes <mkarnes2015@gmail.com>
2017-10-15 21:23:08 -0500

    Initial Commit
    OVR imported
    Spawn zone and voodoo room basic model
    All materials set to black or glass
    LookingGlass and StaticLookingGlass implemented
    Arrow appears when player looks at StaticLookingGlass in spawn zone

---
+++ Assets/Scripts/Looking Glass/LookingGlass.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// A LookingGlass is simply a plane that does something when the player is looking at it
+/// </summary>
+[RequireComponent(typeof(BoxCollider))]
+public abstract class LookingGlass : MonoBehaviour {
+	public Transform CenterEyeAnchor;
+
+	[SerializeField]
+	protected bool physicsBased = true;
+
+	// Use this for initialization
+	protected virtual void Start () {
+	}
+	
+	// Update is called once per frame
+	protected virtual void Update () {
+		if(!physicsBased)
+		{
+			// Find point intersection between plane and looking ray
+			float hitDistance = Vector3.Dot(transform.position - CenterEyeAnchor.position, transform.forward) / Vector3.Dot(transform.forward, CenterEyeAnchor.forward);
+
+			if (hitDistance > 0)
+			{
+				Vector3 hitPoint = CenterEyeAnchor.position + CenterEyeAnchor.forward * hitDistance;
+
+				if (GetComponent<BoxCollider>().bounds.Contains(hitPoint))
+				{
+					Look(hitPoint);
+				}
+			}
+		}
+	}
+
+	public abstract void Look(Vector3 lookPoint);
+}
---
+++ Assets/Scripts/Looking Glass/StaticLookingGlass.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+/// <summary>
+/// A StaticLookingGlass is a LookingGlass that does not perform any transformations
+/// </summary>
+public class StaticLookingGlass : LookingGlass {
+	[SerializeField]
+	public bool PhysicsBased { get { return physicsBased; } set { physicsBased = value; } }
+
+	public UnityEvent OnLook;
+
+	// Use this for initialization
+	protected override void Start () {
+		base.Start();
+	}
+	
+	// Update is called once per frame
+	protected override void Update () {
+		base.Update();
+	}
+
+	public override void Look(Vector3 lookPoint)
+	{
+		OnLook.Invoke();
+	}
+}
