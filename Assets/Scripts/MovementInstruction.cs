4baacae8819e040a1d87671eb5dd5e3353a76242
MichaelKarnes <mkarnes2015@gmail.com>
2017-10-21 14:30:54 -0500

    MovementInstruction and MovementPath
    Initial commit

---
+++ Assets/Scripts/MovementInstruction.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class MovementInstruction : MonoBehaviour {
+	public Vector3 Destination;
+	public bool UseWorldSpace;
+	public float Speed;
+	public UnityEvent OnEnd;
+
+	private Vector3 destinationWorld;
+
+	// Use this for initialization
+	void Start () {
+		destinationWorld = UseWorldSpace ? Destination : transform.TransformPoint(Destination);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 destinationCurrent = destinationWorld - transform.position;
+		Vector3 movementSpeed = destinationCurrent.normalized * Speed * Time.deltaTime;
+
+		// If going at the specified Speed would overshoot the destination, we need to just set the
+		// position equal to the destination
+		if (movementSpeed.magnitude < destinationCurrent.magnitude)
+			transform.position += movementSpeed;
+		else
+			transform.position = destinationWorld;
+
+		if(transform.position == destinationWorld)
+		{
+			OnEnd.Invoke();
+			enabled = false;
+		}
+	}
+
+	private void OnDrawGizmosSelected()
+	{
+		if (enabled)
+		{
+			Gizmos.color = new Color(0f, 0.5f, 1f);
+			Vector3 path;
+			if (UseWorldSpace)
+				path = Destination - transform.position;
+			else
+				path = transform.TransformPoint(Destination) - transform.position;
+			Gizmos.DrawRay(transform.position, path);
+		}
+	}
+}
---
+++ Assets/Scripts/MovementPath.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class MovementPath : MonoBehaviour {
+	public List<Vector3> Nodes;
+	public bool UseWorldSpace;
+	public bool Loop;
+	public float Speed;
+
+	private List<Vector3> NodesWorld = new List<Vector3>();
+	private int currentNodeIndex = 0;
+
+	// Use this for initialization
+	void Start () {
+		if (Nodes.Count > 0)
+			NodesWorld.Add(transform.position);
+
+		foreach(Vector3 node in Nodes)
+			NodesWorld.Add(UseWorldSpace ? node : transform.TransformPoint(node));
+
+		if (Nodes.Count > 0)
+			OnEndPath();
+		else
+			Debug.Log("There must be at least one Node in the MovementPath");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	private void OnEndPath()
+	{
+		currentNodeIndex++;
+
+		if (Loop)
+			currentNodeIndex %= NodesWorld.Count;
+
+		if (currentNodeIndex < NodesWorld.Count)
+		{
+			UnityEvent instructionOnEnd = new UnityEvent();
+			instructionOnEnd.AddListener(OnEndPath);
+
+			MovementInstruction currentInstruction = gameObject.AddComponent<MovementInstruction>();
+			currentInstruction.Destination = NodesWorld[currentNodeIndex];
+			currentInstruction.UseWorldSpace = true;
+			currentInstruction.Speed = Speed;
+			currentInstruction.OnEnd = instructionOnEnd;
+		}
+	}
+
+	private void OnDrawGizmosSelected()
+	{
+		if (enabled)
+		{
+			Gizmos.color = new Color(0f, 0.5f, 1f);
+
+			for (int i = 0; i < Nodes.Count; i++)
+			{
+				Vector3 initPos;
+				Vector3 finalPos;
+
+				if (UseWorldSpace)
+				{
+					initPos = i == 0 ? transform.position : Nodes[i - 1];
+					finalPos = Nodes[i];
+				}
+				else
+				{
+					initPos = i == 0 ? transform.position : transform.TransformPoint(Nodes[i - 1]);
+					finalPos = transform.TransformPoint(Nodes[i]);
+				}
+
+				Gizmos.DrawRay(initPos, finalPos - initPos);
+			}
+
+			if(Loop && Nodes.Count > 0)
+			{
+				Vector3 initPos;
+				Vector3 finalPos;
+
+				if (UseWorldSpace)
+				{
+					initPos = Nodes[Nodes.Count - 1];
+					finalPos = transform.position;
+				}
+				else
+				{
+					initPos = transform.TransformPoint(Nodes[Nodes.Count - 1]);
+					finalPos = transform.position;
+				}
+
+				Gizmos.DrawRay(initPos, finalPos - initPos);
+			}
+		}
+	}
+}
